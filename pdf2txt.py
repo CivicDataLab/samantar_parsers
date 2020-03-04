f54be64d227d6dcdfad906abcc2014ab7aa8911e
akki2825 <akhilesh.rocking@gmail.com>
2020-03-04 17:16:36 +0530

    make standalone script using docopt

--- pdf2txt.py
+++ pdf2txt.py
@@ -1,3 +1,14 @@
+"""
+Script for converting pdfs to text files using tesseract.
+
+Usage:
+    pdf2txt.py --input-folder-path=<if> [--output-folder-path=<of>]
+
+Options:
+    --input-folder-path=<if>            Provide folder path of pdfs to be converted
+    --output-folder-path=<op>           Specify output path for the text files
+"""
+from docopt import docopt
 from pdf2image import convert_from_path
 import tempfile
 from tesserocr import PyTessBaseAPI, PSM
@@ -5,36 +16,38 @@ from tqdm import tqdm
 import os
 
 
-for filename in os.listdir("/home/akhilesh/cdl/pf/data/budgets/19-20/as/pdf/"):
-    with tempfile.TemporaryDirectory() as path:
-        try:
-            images = convert_from_path("/home/akhilesh/cdl/pf/data/budgets/19-20/as/pdf/" + filename, thread_count=8, output_folder=path)
-            ofile = filename.split(".")[0] + ".txt"
+## TODO: Add language support. Right now, its defaulted to English and Assamese at the moment.
 
-            with PyTessBaseAPI(psm=4, lang='eng+asm') as api:
-                with open("/home/akhilesh/cdl/pf/data/budgets/19-20/as/txt/" + ofile, "w+") as f:
-                    for img in tqdm(images):
-                        api.SetImage(img)
-                        f.write(api.GetUTF8Text())
-        except:
-            print("filename: ", filename)
-# for filename in tqdm(os.listdir("/home/akhilesh/cdl/union_budget_suggestions/imgs/")):
-#     with tempfile.TemporaryDirectory() as path:
-#         print(filename)
-#         images = convert_from_path("/home/akhilesh/cdl/union_budget_suggestions/imgs/" + filename, thread_count=8, output_folder=path)
+def converter(ifolder: str, ofolder: str):
+    """
+    convert pdf to text file using tesseract
+    :params ifolder: provide input folder path
+    :params ofolder: provide output folder path
+    :params langs: provide the languages that you would like to recognise
+    """
+    for filename in os.listdir(ifolder):
+        with tempfile.TemporaryDirectory() as path:
+            try:
+                images = convert_from_path(ifolder + filename, thread_count=8, output_folder=path)
+                ofile = filename.split(".")[0] + ".txt"
 
-#         ofile = filename.split(".")[0] + ".txt"
-#         with PyTessBaseAPI(psm=4, lang='hin+eng') as api:
-#             with open("/home/akhilesh/cdl/union_budget_suggestions/txts_imgs/" + ofile, "w+") as f:
-#                 for img in images:
-#                     api.SetImage(img)
-#                     f.write(api.GetUTF8Text())
+                with PyTessBaseAPI(psm=4, lang='eng+asm') as api:
+                    with open(ofolder + ofile, "w+") as f:
+                        for img in tqdm(images):
+                            api.SetImage(img)
+                            f.write(api.GetUTF8Text())
+            except:
+                print(f"{filename} file not converted")
 
 
-# images = os.listdir("/home/akhilesh/cdl/union_budget_suggestions/imgs/")
-# for img in tqdm(images):
-#     ofile = img.split(".")[0] + ".txt"
-#     with open("/home/akhilesh/cdl/union_budget_suggestions/txts_imgs/" + ofile, "w+") as f:
-#         with PyTessBaseAPI(psm=4, lang='hin+eng') as api:
-#             api.SetImageFile(img)
-#             f.write(api.GetUTF8Text())
+if __name__ == "__main__":
+    args = docopt(__doc__)
+
+    ifolder = args["--input-folder-path"]
+
+    ofolder = args["--output-folder-path"]
+
+    if not ofolder:
+        ofolder = os.path.dirname(os.path.realpath(__file__))
+
+    converter(ifolder, ofolder)
